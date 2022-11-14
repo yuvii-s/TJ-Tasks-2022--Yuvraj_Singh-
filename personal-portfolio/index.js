fda6d2213a04f3541b96cdfc94adba5154be3fa6
yuvii-s <4ikcode@gmail.com>
2022-11-14 21:19:20 +0530

    site
    whole site

---
+++ personal-portfolio/index.js
@@ -0,0 +1,16 @@
+"use strict";
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+let darkmode = document.querySelector("#darkmode");
+console.log(darkmode);
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
